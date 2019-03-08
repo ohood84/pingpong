66c4e0b656efa2226ff1a7faed2950c7bc7f80ad
ohood84 <obeidat.ohood@gmail.com>
2019-03-08 10:08:36 -0800

    it will add div with jumbotron class to align the buttons in center

--- js/scripts.js
+++ js/scripts.js
@@ -112,7 +112,7 @@ $(document).ready(function(){
 
       var printarray=function(){
         $("#result").empty();
-        for (var i=counter1;i<counter2;i++){
+        for (var i=counter1;i<=counter2;i++){
           $("ul#result").append("<li>"+pingpongarray[i]+"</li>");
           if(pingpongarray[i]==="ping"){
               $("li").last().addClass("ping");
