fde71029c65e47b6044c274facbe122aa161e57b
ohood84 <obeidat.ohood@gmail.com>
2019-03-07 20:57:12 -0800

    it will creat array and put the data in it

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-  $(".centered").fadeIn("3000");
+
   var counter=0,number;
+  var pingpongarray=[];
     // while(counter<3){
     $("#pingpong").click(function(event){
       $("#result").empty();
@@ -8,9 +9,11 @@ $(document).ready(function(){
       // alert(number);
       pingpong(number);
       // $("#result").text(number);
+      printarray();
      event.preventDefault();
 
     });
+
     $("#result").slideDown();
     // $("#ping").show();
     // $("#pong").show();
@@ -29,28 +32,39 @@ $(document).ready(function(){
 
        var findpingpong =function(num){
             if((num%3===0)&&(num%5===0)){
-             // $("pingpongclass").show();
+             $("pingpongclass").show();
+             pingpongarray[num]="pingpong";
            }
              else if (((num%3)===0)){
                  // $("li").removeClass();
 
                  // $("li").css("color","green")
-               $("ul#result").append("<li>ping</li>");
-                $("li").last().addClass("pong");
+               // $("ul#result").append("<li>ping</li>");
+               pingpongarray[num] =("<li>ping</li>");
+                  // $("li").last().addClass("ping");
+                  // ]="ping";
+
                // consol.log();
             }
               else if (((num%5)===0)){
                 // $("li").removeClass();
-                $("ul#result").append("<li>pong</li>");
-                $("li").last().addClass("ping");
+                // $("ul#result").append("<li>pong</li>");
+               // $("li").last().addClass("pong");
+                   pingpongarray[num]=("<li>pong</li>");
              }
                else{
                 // $("li").removeClass();
-                $("ul#result").append("<li>"+num+"</li>");
+                // $("ul#result").append("<li>"+num+"</li>");
+                   pingpongarray[num]=("<li>"+num+"</li>");
               }
               };
 
-
+      var printarray=function(){
+        $("#result").empty();
+        for (var i=1;i<=10;i++){
+          $("ul#result").append(pingpongarray[i]);
+        }
+      }
 
 
     });
